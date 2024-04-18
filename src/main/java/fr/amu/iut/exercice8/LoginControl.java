a6728871e2f74016ee3c73627bef8e0ec198f52b
Théotime Lecuyer <theotime.lecuyer@etu.univ-amu.fr>
2024-04-18 12:11:24 +0200

    TD/TP 3 - Exo. 8

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,27 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
+    @FXML
+    private TextField uid;
+
+    @FXML
+    private PasswordField pwd;
+
+    @FXML
     private void okClicked() {
+        System.out.println(uid.getText());
+        System.out.println("*".repeat(pwd.getText().length()));
     }
 
+    @FXML
     private void cancelClicked() {
+        uid.setText("");
+        pwd.setText("");
     }
 }
