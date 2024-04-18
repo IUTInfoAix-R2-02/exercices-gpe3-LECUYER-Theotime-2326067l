3b7a4168be8e18c9281290b9595c41d0e6cff04b
Théotime Lecuyer <theotime.lecuyer@etu.univ-amu.fr>
2024-04-18 10:48:21 +0200

    TD/TP 3 - Exo. 7

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,7 +1,11 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,14 +13,28 @@ public class CounterController implements Initializable {
 
     int counter = 0;
 
+    @FXML
+    private Label counterLabel;
+
+    @FXML
+    private Button decrementButton;
+
+    @FXML
+    private Button incrementButton;
+
     public void increment() {
+        ++counter;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     public void decrement() {
+        --counter;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing CounterController...");
+        counterLabel.setText("0");
    }
 }
